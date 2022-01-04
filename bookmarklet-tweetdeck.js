029ef963b77001ba2af13f6a634dde1047ee6366
rtaro02 <nmeeaiwxntwsoetnlelin@gmail.com>
2022-01-04 09:55:08 +0900

    update

---
+++ bookmarklet-tweetdeck.js
@@ -0,0 +1,20 @@
+javascript:(function(){
+    let a = document. querySelectorAll("a");
+    for(var i = 0; i < a.length; i++) {
+            if(a[i].hasAttribute("class") && a[i].getAttribute("class") === 'js-action-header-button column-header-link column-settings-link'){
+                  a[i].click();
+            }
+    }
+    let b = document. querySelectorAll("button");
+    for(var i = 0; i < b.length; i++) {
+            if(b[i].hasAttribute("class") && b[i].getAttribute("data-action") === 'clear'){
+                  b[i].click();
+            }
+    }
+    for(var i = 0; i < a.length; i++) {
+            if(a[i].hasAttribute("class") && a[i].getAttribute("class") === 'js-action-header-button column-header-link column-settings-link'){
+                  a[i].click();
+            }
+    }
+    setTimeout(location.reload(), 1000);
+})();
